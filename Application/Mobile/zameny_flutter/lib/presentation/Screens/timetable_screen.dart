9edcf3246b2d123d2e84ff763821ae094a4f88b1
EventGamer67 <islamoffdanil67@gmail.com>
2024-04-01 00:33:44 +0500

    clean

--- Application/Mobile/zameny_flutter/lib/presentation/Screens/timetable_screen.dart
+++ Application/Mobile/zameny_flutter/lib/presentation/Screens/timetable_screen.dart
@@ -1,9 +1,5 @@
 import 'dart:async';
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
