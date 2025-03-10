93a38ac24dc95eb64cd21d39b5a467b224d95519
mdktdys <islamoffdanil67@gmail.com>
2025-03-10 22:59:50 +0500

    conditional import

--- Application/Mobile/zameny_flutter/lib/shared/providers/navigation/navigation_provider.dart
+++ Application/Mobile/zameny_flutter/lib/shared/providers/navigation/navigation_provider.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:zameny_flutter/shared/providers/navigation/navigation_stub.dart'
-    if (dart.library.io) 'package:zameny_flutter/services/navigation/navigation_android.dart'
-    if (dart.library.html) 'package:zameny_flutter/services/navigation/navigation_web.dart';
+    if (dart.library.io) 'package:zameny_flutter/shared/providers/navigation/navigation_android.dart'
+    if (dart.library.html) 'package:zameny_flutter/shared/providers/navigation/navigation_web.dart';
 
 final navigationProvider = ChangeNotifierProvider<Navigation>((final ref) {
   return Navigation();
