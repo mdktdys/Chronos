d982a51d837ec5b3ca2e982a6e9f448aa709a3d8
mdktdys <islamoffdanil67@gmail.com>
2025-04-21 02:48:25 +0500

    update 21.04.25

--- Application/Mobile/zameny_flutter/lib/modules/schedule/presentation/widgets/search_result_header.dart
+++ Application/Mobile/zameny_flutter/lib/modules/schedule/presentation/widgets/search_result_header.dart
@@ -1,7 +1,5 @@
-import 'dart:developer';
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_animate/flutter_animate.dart';
